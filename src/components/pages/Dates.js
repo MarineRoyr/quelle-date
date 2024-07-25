f2fa2ca63f551e6e827711c28698ae684ec9db96
Marine Royer <marine.royr@gmail.com>
2024-07-25 18:00:21 +0200

    configuration images environnement

--- src/components/pages/Dates.js
+++ src/components/pages/Dates.js
@@ -3,6 +3,14 @@ import { useLocation } from 'react-router-dom';
 import data from '../../data/dates.json';
 import '../../styles/dates.scss';
 
+// Déterminez si vous êtes en développement ou en production
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+// Configurez le chemin d'image basé sur l'environnement
+const imageUrl = isDevelopment
+  ? ''  // Chemin relatif pour le développement
+  : `${process.env.REACT_APP_IMAGE_PATH}`;  // Chemin complet pour la production
+
 const DateComponent = () => {
   const [results, setResults] = useState([]);
   const [searchYear, setSearchYear] = useState('');
@@ -38,35 +46,38 @@ const DateComponent = () => {
                 <div>
                   <div className="eventLeft">
                     <div className="eventYearList">
-                      <img src="/images/evenement.png"  alt="icône"/>
-                    <div className='eventTitle'><h3>L'évènement</h3>
-                  <p> {event.evenement}</p> </div>
+                      <img src={`${imageUrl}/images/evenement.png`} alt="icône"/>
+                      <div className='eventTitle'>
+                        <h3>L'évènement</h3>
+                        <p>{event.evenement}</p>
+                      </div>
                     </div>
                     {event.personnage && (
                       <div className="personnage">
                         <div className="personnagePhoto">
-                          <img src={event.personnage.photoPersonnage} alt={event.personnage.name} />
+                          <img src={`${imageUrl}/${event.personnage.photoPersonnage}`} alt={event.personnage.name} />
                         </div>
                         <div className='personnageTxt'>
-                        <h3 className="personnageName">{event.personnage.name}</h3>
-                        <div className="personnageDescription">{event.personnage.description}
+                          <h3 className="personnageName">{event.personnage.name}</h3>
+                          <div className="personnageDescription">
+                            {event.personnage.description}
+                          </div>
+                          <div className="mediaLinks">
+                            <p>Pour les plus curieux !
+                              <br />Découvrez ce personnage d'un autre temps avec nos ressources d'aujourd'hui
+                            </p>
+                            {event.personnage.podcastUrl && (
+                              <button onClick={() => window.open(event.personnage.podcastUrl, '_blank')}>
+                                LE Podcast
+                              </button>
+                            )}
+                            {event.personnage.youtubeUrl && (
+                              <button onClick={() => window.open(event.personnage.youtubeUrl, '_blank')}>
+                                LA Vidéo YouTube
+                              </button>
+                            )}
+                          </div>
                         </div>
-                        <div className="mediaLinks">
-                        <p> Pour les plus curieux !
-                                <br/>Découvrez ce personnage d'un autre temps avec nos ressources d'aujourd'hui</p>
-                  {event.personnage.podcastUrl && (
-                    <button onClick={() => window.open(event.personnage.podcastUrl, '_blank')}>
-                     LE Podcast
-                    </button>
-                  )}
-                  {event.personnage.youtubeUrl && (
-                    <button onClick={() => window.open(event.personnage.youtubeUrl, '_blank')}>
-                     LA Vidéo YouTube
-                    </button>
-                  )}
-                </div>
-                        </div>
-                       
                       </div>
                     )}
                   </div>
@@ -74,7 +85,7 @@ const DateComponent = () => {
               </div>
               <div className="eventRight">
                 <h2>L'année {searchYear}</h2>
-                <img  className="banner" src={event.bannerImage} alt="Bannière thématique" />
+                <img className="banner" src={`${imageUrl}/${event.bannerImage}`} alt="Bannière thématique" />
               </div>
             </div>
           ))}
